2fcb0de7704bef5b4c7823a635e1cf2aa4db0888
Chris Adams <chris@productscience.co.uk>
2024-11-28 18:58:45 +0100

    Update weekly import when there is no last_state yet

--- f/weekly_imports/fetch_latest_signups.py
+++ f/weekly_imports/fetch_latest_signups.py
@@ -12,7 +12,13 @@ import json
 import wmill
 import gspread
 import rich
+import logging
 
+from rich.logging import RichHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(RichHandler())
+logger.setLevel(logging.DEBUG)
 
 # You can import any PyPi package.
 # See here for more info: https://www.windmill.dev/docs/advanced/dependencies_in_python
@@ -22,10 +28,22 @@ def main(gsheet_key: str, worksheet_name: str, override_email: str = None):
     if override_email:
         LAST_EMAIL_ADDRESS = override_email
     else:
+        # check for either the last run state or initial variable value
         last_state = wmill.get_state()
+        # if we have no last state to refer to, we look for 
+        # email stored in the resource in its place
+        if not last_state:
+            resource_path = "f/weekly_imports/last_email_address_object"
+            logger.debug((
+                f"No last state found. Fetching email from resource {resource_path} "
+                "as a fallback.")
+                )
+            last_state = wmill.get_resource("f/weekly_imports/last_email_address_object")
+            logger.debug(f"Email from resource {resource_path}: {last_state}")
+
         LAST_EMAIL_ADDRESS = last_state.get("last_email_address")
 
-    rich.print(f"Current Last email address: {LAST_EMAIL_ADDRESS}")
+    logger.info(f"Current Last email address: {LAST_EMAIL_ADDRESS}")
 
     path_to_resource = "f/weekly_imports/improving_c_gspread_service_account"
     gspread_cred_dict = wmill.get_resource(path_to_resource)
@@ -37,14 +55,14 @@ def main(gsheet_key: str, worksheet_name: str, override_email: str = None):
     last_row = member_dicts[-1]
 
     if not last_row:
-        rich.print("No new signups to pass along")
+        logger.info("No new signups to pass along")
         return []
 
     if last_row:
         last_email = last_row.get("Email Address")
 
         if last_email:
-            rich.print(f"Setting last email address to: {last_email}")
+            logger.info(f"Setting last email address to: {last_email}")
             new_state = {"last_email_address": last_email}
             wmill.set_state(new_state)
 
@@ -64,7 +82,7 @@ def fetch_latest_signups(
     last_match = responses_worksheet.findall(last_address)[-1]
     # return the rows after the matching email address
     header_rows = responses_worksheet.row_values(1)
-    rich.print(f"Header rows from the worksheet: {header_rows}")
+    logger.debug(f"Header rows from the worksheet: {header_rows}")
     matching_rows = responses_worksheet.get_values(f"A{last_match.row + 1}:ZZ")
 
     member_dicts = []
