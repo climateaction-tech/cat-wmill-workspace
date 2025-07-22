439af1fc1ce84290a42fbeeb589af078e7630176
Chris Adams <chris@productscience.co.uk>
2025-07-22 14:34:55 +0200

    Add dotenv for running locally with .env file

--- notebooks/create-buttondown-newsletter.py
+++ notebooks/create-buttondown-newsletter.py
@@ -1,19 +1,17 @@
 import marimo
 
-__generated_with = "0.9.23"
+__generated_with = "0.14.10"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md(
         r"""
-        # Creating our next Sunday newsletter
+    # Creating our next Sunday newsletter
 
-        This script creates our next CAT newsletter with Buttondown. It's expected to be run on the Sunday morning we send it out.
-
-
-        """
+    This script creates our next CAT newsletter with Buttondown. It's expected to be run on the Sunday morning we send it out.
+    """
     )
     return
 
@@ -22,6 +20,9 @@ def __(mo):
 def _():
     import marimo as mo
     import os
+
+    from dotenv import load_dotenv, dotenv_values
+    load_dotenv(".env")
     return mo, os
 
 
@@ -35,10 +36,10 @@ def _(os):
 def _(mo):
     mo.md(
         r"""
-        ### which emails have we sent already?
+    ### which emails have we sent already?
 
-        We need a list of emails sent from the Buttondown API. The emails are ordered by their creation date in descending order, and the body field is excluded from the response, because it totally bloats the payload size.
-        """
+    We need a list of emails sent from the Buttondown API. The emails are ordered by their creation date in descending order, and the body field is excluded from the response, because it totally bloats the payload size.
+    """
     )
     return
 
@@ -60,17 +61,17 @@ def _(BUTTONDOWN_API_KEY):
 
     response = httpx.get(url, headers=headers, params=payload)
     response
-    return headers, httpx, payload, response, url
+    return headers, httpx, response, url
 
 
 @app.cell
 def _(mo):
     mo.md(
         r"""
-        ### What does our response look like?
+    ### What does our response look like?
 
-        Let's have a look. We have our IDs, and the titles. These are what we want to refer to when creating our draft emails later.
-        """
+    Let's have a look. We have our IDs, and the titles. These are what we want to refer to when creating our draft emails later.
+    """
     )
     return
 
@@ -86,10 +87,10 @@ def _(response):
 def _(mo):
     mo.md(
         r"""
-        ### Ok, when is the next newsletter supposed to go out?
+    ### Ok, when is the next newsletter supposed to go out?
 
-        We need this so we can decide to either create a draft newsletter, or if there is already is an existing newsletter, figure out what to do with it.
-        """
+    We need this so we can decide to either create a draft newsletter, or if there is already is an existing newsletter, figure out what to do with it.
+    """
     )
     return
 
@@ -118,11 +119,11 @@ def _(newsletters):
 
         # find the week we are in, and add one week to it, and return the sunday
         _new_issue_datetime = _extracted_date + datetime.timedelta(weeks=1)  # next Sunday
-        
+
         return _new_issue_datetime.date()
 
     get_next_sunday(newsletters)
-    return calendar, datetime, get_next_sunday, re
+    return get_next_sunday, re
 
 
 @app.cell
@@ -132,28 +133,22 @@ def _(get_next_sunday, newsletters, re):
         last_subject = newsletters[0]
         issue_number = int(re.search(r'\d+', last_subject).group()) + 1
         _new_issue_title = f"🌍 CAT Newsletter {issue_number} - {sunday_date}"
-        
+
         if _new_issue_title in [n for n in newsletters]:
             raise ValueError("A newsletter with this subject already exists.")
         return _new_issue_title
     new_issue_title = create_new_subject(newsletters)
-    return create_new_subject, new_issue_title
+    return (new_issue_title,)
 
 
 @app.cell
-def __(mo):
-    mo.md(
-        r"""
-        ## OK, now we create our new newsletter, using the API, and `new_issue_title`, and fetching the most recently created newsletter file locally.
-
-
-        """
-    )
+def _(mo):
+    mo.md(r"""## OK, now we create our new newsletter, using the API, and `new_issue_title`, and fetching the most recently created newsletter file locally.""")
     return
 
 
-@app.cell(disabled=True)
-def __(headers, httpx, latest_newsletter_text, new_issue_title, url):
+@app.cell
+def _(headers, httpx, latest_newsletter_text, new_issue_title, url):
     newsletter_text = ""
     with open(f"./{latest_newsletter_text}") as newsletter_file:
         newsletter_text = newsletter_file.read()
@@ -166,30 +161,25 @@ def __(headers, httpx, latest_newsletter_text, new_issue_title, url):
 
     create_response = httpx.post(url, headers=headers, json=new_newsletter_payload)
     create_response
-    return (
-        create_response,
-        new_newsletter_payload,
-        newsletter_file,
-        newsletter_text,
-    )
+    return
 
 
 @app.cell
-def __():
+def _():
     import pathlib
 
     matching_newsletter_files = [
         entry for entry in 
         pathlib.Path(".").glob("*cat-generated-newsletter*")
     ]
-        
+
     latest_newsletter_text = sorted(matching_newsletter_files)[-1]
     latest_newsletter_text
-    return latest_newsletter_text, matching_newsletter_files, pathlib
+    return (latest_newsletter_text,)
 
 
 @app.cell
-def __():
+def _():
     return
 
 
